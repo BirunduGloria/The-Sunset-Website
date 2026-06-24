0cfc11afc167743ab4fd1636484abb04f368ccde
birundugloria <birundugloria@gmail.com>
2026-06-24 14:01:59 +0300

    feat: add room data and findRoomByPrice function to manage room listings

---
+++ server/data/rooms.js
@@ -0,0 +1,27 @@
+export const ROOMS = [
+  {
+    name: "Standard Room",
+    price: 100,
+    image: "/images/pexels-didsss.jpg",
+    description:
+      "A cozy and affordable room perfect for solo travelers or short stays.",
+  },
+  {
+    name: "Deluxe Room",
+    price: 180,
+    image: "/images/polinaunsplash.jpg",
+    description:
+      "A spacious room designed for comfort and relaxation. Ideal for couples and business travelers seeking a premium stay.",
+  },
+  {
+    name: "Ocean Suite",
+    price: 250,
+    image: "/images/pexels-andreaedavis-30018003.jpg",
+    description:
+      "Experience luxury with breathtaking ocean views, elegant interiors, and premium amenities crafted for unforgettable stays.",
+  },
+];
+
+export function findRoomByPrice(price) {
+  return ROOMS.find((room) => room.price === Number(price));
+}
