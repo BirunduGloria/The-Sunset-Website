42be2754fe1a8d63c3aeb97a212be714a0485f08
birundugloria <birundugloria@gmail.com>
2026-06-25 14:16:25 +0300

    feat: restructure routing to include Home, Contact, and RoomDetails components

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import RoomDetails from "./pages/RoomDetails";
 import Admin from "./Admin.jsx";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/*" element={<App />} />
+
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/room/:roomName"
+          element={<RoomDetails />}
+        />
+
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+
+        <Route
+          path="/admin"
+          element={<Admin />}
+        />
+
       </Routes>
     </BrowserRouter>
   </StrictMode>
